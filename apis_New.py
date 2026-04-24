e04c6e95f9c34be16f05db6c27f5bf7a72679638
XavZol <alansolis707@gmail.com>
2026-04-24 05:11:48 -0600

    Manejo de Errores.

--- apis_New.py
+++ apis_New.py
@@ -2,27 +2,15 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 
-url = "https://jsonplaceholder.typicode.com/posts/1"
+url = "https://jsonplaceholder.typicode.com/posts/a"
 
 respuesta = requests.get(url)
-data = respuesta.json()
-data["title"]
-titulo1 = data["title"]
-print(titulo1)
 
-url2 = "https://jsonplaceholder.typicode.com/posts/"
-respuesta2 = requests.get(url2)
-data2 = respuesta2.json()
-print(data2)
+print(respuesta.status_code)
 
-usuarios = []
-
-for post in data2:
-    usuarios.append(post["userId"])
-print(usuarios)
-
-miSerie = pd.Series(usuarios)
-print(miSerie)
-
-plt.boxplot(usuarios)
-plt.show()
+if respuesta.status_code == 200:
+    data = respuesta.json()
+    print(data)
+else:
+    print(f"Error {respuesta.status_code}: no disponemos de esa información")
+    
